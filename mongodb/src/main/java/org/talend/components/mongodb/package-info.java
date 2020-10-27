9c69f505f1e95ea2429edd917bc1629f610d3a82
ypiel <ypiel@talend.com>
2020-10-27 18:42:18 +0100

    feat(TDI-42337) mongodb icons

--- mongodb/src/main/java/org/talend/components/mongodb/package-info.java
+++ mongodb/src/main/java/org/talend/components/mongodb/package-info.java
@@ -1,7 +1,7 @@
 // this tells the framework in which family (group of components) and categories (UI grouping)
 // the components in the nested packages belong to
 @Components(family = "MongoDB", categories = "Databases NoSQL")
-@Icon(value = Icon.IconType.CUSTOM, custom = "mongodb")
+@Icon(value = Icon.IconType.CUSTOM, custom = "mongo_db")
 package org.talend.components.mongodb;
 
 import org.talend.sdk.component.api.component.Components;
